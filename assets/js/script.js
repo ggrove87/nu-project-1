550cdccfa38739e8bd0e4922958d6ba79a7925d6
nearcatch <692914+nearcatch@users.noreply.github.com>
2022-04-12 12:45:28 -0500

    store searches to localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 let inputForm = document.querySelector("#inputForm");
 let enteredMovie;
+// read movieSearches from localStorage if available, otherwise set to empty array
+let movieSearches = JSON.parse(localStorage.getItem("movieSearches"));
+if (movieSearches === null) {
+    movieSearches = [];
+}
+// set data from each fetch to a var for easy manipulation
+let movieData;
+let youtubeData;
 inputForm.addEventListener("submit", function (event) {
     event.preventDefault();
     enteredMovie = document.querySelector("#enteredMovie").value;
@@ -15,26 +23,37 @@ function pullMovieInfo() {
     )
         .then((response) => response.json())
         .then((data) => {
+            movieData = data;
             console.log(data);
             console.log(data.imdbID);
-            // storeMovieData(data)
+            storeMovieSearch(data, enteredMovie);
         });
 }
 
 function pullMovieTrailer() {
     let apiKeyYouTube = "AIzaSyAqFrtQreRkV1LkaZO8evfjc0ArN7GeCv4";
     fetch(
-        `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${enteredMovie}&key=${apiKeyYouTube}`,
+        `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${enteredMovie}+trailer&key=${apiKeyYouTube}`,
         {
             header: "Accept: application/json",
         }
     )
         .then((response) => response.json())
         .then((data) => {
+            youtubeData = data;
             console.log(data.items[0].id.videoId);
         });
 }
 
-function storeMovieData(data) {
-    localStorage.setItem(data.imdbID, enteredMovie);
+function storeMovieSearch(data, search) {
+    // build object for the current search
+    search = search.toLowerCase();
+    currentSearch = {
+        imdb: data.imdbID,
+        search: search,
+    };
+    movieSearches.unshift(currentSearch);
+    console.log(movieSearches);
+    // submit to localStorage
+    localStorage.setItem("movieSearches", JSON.stringify(movieSearches));
 }
